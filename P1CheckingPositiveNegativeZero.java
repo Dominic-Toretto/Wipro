76d815e058caa661c5a28e5f21b38084a413d8ca
unknown <shinde23897m@gmail.com>
2022-04-13 10:30:28 +0530

    first commit

---
+++ P1CheckingPositiveNegativeZero.java
@@ -0,0 +1,14 @@
+public class P1CheckingPositiveNegativeZero {
+    public static void main(String args[]){
+        int m=0;
+        if(m>0){
+            System.out.println("Number is Positive");
+        }
+        else if(m<0){
+            System.out.println("Number is Negative");
+        }
+        else{
+            System.out.println("Number is Zero");
+        }
+    }
+}
